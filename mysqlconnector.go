4bdf9456adb822035e35caf476157ac1224d53a0
ssola <introduccio@gmail.com>
2016-03-01 21:59:14 +0100

    get credentials from environment

--- mysqlconnector.go
+++ mysqlconnector.go
@@ -6,10 +6,14 @@ import (
     "database/sql"
     "log"
     "github.com/confluentis/api/entities"
+    "fmt"
+    "os"
 )
 
 func initDb() *gorp.DbMap {
-    db, err := sql.Open("mysql", "root:123@tcp(192.168.33.99:3306)/confluentis")
+    mysqlconnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("API_MYSQL_USER"), os.Getenv("API_MYSQL_PASSWORD"), os.Getenv("API_MYSQL_HOST"), os.Getenv("API_MYSQL_PORT"), os.Getenv("API_MYSQL_DATABASE"))
+
+    db, err := sql.Open("mysql", mysqlconnection)
 
     if err != nil {
         log.Fatal("Failed connection", err)
